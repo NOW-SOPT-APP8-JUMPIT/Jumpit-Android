cba427ec674c5c8722c53976b63ff166c3b9b307
gaeulzzang <gaeulzzang@gmail.com>
2024-05-14 18:26:25 +0900

    feat: 도움말 팝업창 구현

--- app/src/main/java/com/sopt/now/jumpit/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/now/jumpit/ui/main/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.sopt.now.jumpit.ui.main
 
+import android.os.Bundle
+import android.text.TextUtils.replace
 import com.sopt.now.jumpit.R
 import com.sopt.now.jumpit.databinding.ActivityMainBinding
 import com.sopt.now.jumpit.ui.base.BindingActivity
+import com.sopt.now.jumpit.ui.resume.ResumeFragment
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.clMain, ResumeFragment())
+//            .commit()
+//    }
 }
--- app/src/main/java/com/sopt/now/jumpit/ui/resume/ResumeFragment.kt
+++ app/src/main/java/com/sopt/now/jumpit/ui/resume/ResumeFragment.kt
@@ -1,18 +1,63 @@
 package com.sopt.now.jumpit.ui.resume
 
-import androidx.fragment.app.viewModels
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.ViewDataBinding
+import android.widget.PopupWindow
 import com.sopt.now.jumpit.R
 import com.sopt.now.jumpit.databinding.FragmentResumeBinding
 import com.sopt.now.jumpit.ui.base.BindingFragment
 
-class ResumeFragment() : BindingFragment<ViewDataBinding>(R.layout.fragment_resume) {
+class ResumeFragment : BindingFragment<FragmentResumeBinding>(R.layout.fragment_resume) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onResumeHelpBtnClick()
+        onAttachmentHelpBtnClick()
+    }
+
+    private fun onResumeHelpBtnClick() {
+        binding.btnResumeHelp.setOnClickListener {
+            val originalImage = binding.btnResumeHelp.drawable
+
+            PopupWindow(requireContext()).apply {
+                contentView = layoutInflater.inflate(R.layout.popup_resume, null)
+                setBackgroundDrawable(ColorDrawable(Color.WHITE))
+                isFocusable = true
+                showAsDropDown(binding.btnResumeHelp)
+
+                setOnDismissListener {
+                    binding.btnResumeHelp.setImageDrawable(originalImage)
+                }
+
+                contentView.setOnTouchListener { _, _ ->
+                    dismiss()
+                    true
+                }
+            }
+            binding.btnResumeHelp.setImageResource(R.drawable.icn_help_select)
+        }
+    }
+
+    private fun onAttachmentHelpBtnClick() {
+        binding.btnAttachmentHelp.setOnClickListener {
+            val originalImage = binding.btnAttachmentHelp.drawable
+            PopupWindow(requireContext()).apply {
+                contentView = layoutInflater.inflate(R.layout.popup_attachment, null)
+                setBackgroundDrawable(ColorDrawable(Color.WHITE))
+                isFocusable = true
+                showAsDropDown(binding.btnAttachmentHelp)
+
+                setOnDismissListener {
+                    binding.btnAttachmentHelp.setImageDrawable(originalImage)
+                }
+
+                contentView.setOnTouchListener { _, _ ->
+                    dismiss()
+                    true
+                }
+            }
+            binding.btnAttachmentHelp.setImageResource(R.drawable.icn_help_select)
+        }
     }
 }
