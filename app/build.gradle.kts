3d6597b5c0f1ca0837725146cab7cc14eb2040c2
junseo511 <prime54@g.skku.edu>
2024-05-17 19:10:47 +0900

    build: Navigation 의존성 버전카탈로그 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,5 @@ dependencies {
     implementation(libs.bundles.coil)
 
     // Navigation
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation (libs.bundles.navigation)
 }
