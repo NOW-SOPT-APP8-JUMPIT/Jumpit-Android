e80f7a49922efb29aa6dbab8ecd8b0773b715568
nagaeng <helen030816@naver.com>
2024-05-22 10:58:52 +0900

    feat: 홈 UI 구현 3차 검색 클릭 시 SearchFragment 전환 구현

--- app/src/main/java/com/sopt/now/jumpit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/now/jumpit/ui/home/HomeFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.sopt.now.jumpit.R
+import com.sopt.now.jumpit.ui.search.SearchFragment
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +17,14 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val searchButton: ImageView = view.findViewById(R.id.searchBar)
+        searchButton.setOnClickListener {
+            findNavController().navigate(R.id.actionHomeToSearch)
+        }
     }
 }
