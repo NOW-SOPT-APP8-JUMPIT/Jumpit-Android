410f4254205c1a080e0477c020f4e90e8701dde1
junseo511 <prime54@g.skku.edu>
2024-05-24 15:30:00 +0900

    fix: 그래들 빌드 오류 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.kotlinSerialization) apply false
 }
