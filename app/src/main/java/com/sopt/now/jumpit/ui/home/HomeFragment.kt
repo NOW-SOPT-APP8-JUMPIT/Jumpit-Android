696facd0dadcb12348af08a71f01a9eb74f553b3
nagaeng <helen030816@naver.com>
2024-05-24 14:27:48 +0900

    feat: HomeFragment - BindingFrament 상속 형식으로 수정

--- app/src/main/java/com/sopt/now/jumpit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/now/jumpit/ui/home/HomeFragment.kt
@@ -1,29 +1,17 @@
 package com.sopt.now.jumpit.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.sopt.now.jumpit.R
-import com.sopt.now.jumpit.ui.search.SearchFragment
+import com.sopt.now.jumpit.databinding.FragmentHomeBinding
+import com.sopt.now.jumpit.ui.base.BindingFragment
 
-class HomeFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val searchButton: ImageView = view.findViewById(R.id.searchBar)
-        searchButton.setOnClickListener {
+        binding.searchBar.setOnClickListener {
             findNavController().navigate(R.id.actionHomeToSearch)
         }
     }
